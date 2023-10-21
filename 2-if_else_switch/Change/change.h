a7a84d48856e5a24b5641354e8b748f0bb7a8e3e
NikitaOrehov <Ornik0005@yandex.ru>
2023-10-21 15:52:56 +0300

    111

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,9 +1,9 @@
 long long change(int a, int b, int c, int d){
     long long _c = c;
     long long int h = 100 * a + b, f;
-    if (h > (100 * c + d)) return -1;
+    if (h > (100 * _c + d)) return -1;
     else {
-        f = (100 * c + d) - (100 * a + b);
+        f = (100 * _c + d) - (100 * a + b);
         return (f / 100) * 100 + (f % 100);
     }
 }
