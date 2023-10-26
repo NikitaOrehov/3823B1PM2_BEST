f3c5c4742697742f699f1ada94af65adcc28f21b
NikitaOrehov <Ornik0005@yandex.ru>
2023-10-27 01:52:05 +0300

    222

--- 4-array/find_hole/hole_finder.h
+++ 4-array/find_hole/hole_finder.h
@@ -1,5 +1,14 @@
 #include <math.h>
 
 int hole_finder(int matrix[], int length, int width){
-    return 0;
+    int min = 1000, res;
+    for (int i = 0; i < width; i++){
+        for (int j = 0; j < length; j ++){
+            if (matrix[j + (length * i)] < min){
+                min = matrix[j + (length * i)];
+                res = i * 100 + j;
+            }
+        }
+    }
+    return res;
 }
