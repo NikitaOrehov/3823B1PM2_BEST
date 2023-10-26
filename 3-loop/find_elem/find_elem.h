4384aec662114041a8379f16fc3a456a9eff7e8d
NikitaOrehov <Ornik0005@yandex.ru>
2023-10-26 17:53:09 +0300

    26.10.2023

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -1,8 +1,23 @@
 #include <math.h>
 
 int find_elem(int left, int right, int number, int max_iter){
+    int a = left;
     int i = 0; //Счётчик итераций
+    int middle;
+    while (left <= right){
+        i += 1;
+        middle = (left + right) / 2;
+        if (number > middle){
+            left = middle + 1;
+        }
+        else if (number == middle){
+            return ((number - a) * i);
+        }
+        else{
+            right = middle - 1;
+        }
 
+    }
 
     return i; //НЕ МЕНЯТЬ!
 }
--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -1,3 +1,15 @@
 int maxmin(int input[], int size){
-    return -1;
+    int count = 0;
+    for (int i = 1; i < size - 1; i++)
+        if ((input[i] < input[i - 1]) && (input[i] < input[i + 1]) && (input[i] > count)){
+            count = input[i];
+        }
+    if ((input[0] < input[1]) && input[0] > count){
+        count = input[0];
+    }
+    if ((input[size - 1] < input[size - 2]) && input[size - 1] > count){
+        count = input[0];
+
+    }
+    return count;
 }
