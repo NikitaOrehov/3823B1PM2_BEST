86eab305ac7803362518ecf3299f3f0b4b72ba7f
NikitaOrehov <Ornik0005@yandex.ru>
2023-10-28 01:31:13 +0300

    28.10.2023

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,5 +1,30 @@
 #include <math.h>
 
 char find_digit(double number, char digit){
-    return 100;
+    if (digit >= 10) return -1;
+    int number2 = (int)number, i = 0;
+    char k = 0, count = 0;
+    while (number2 >= 1){
+        i += 1;
+        k = number2 % 10;
+        number2 = number2 / 10;
+        if (count >= 1){
+            count += 1;
+        }
+        if (k == digit){
+            count = 1;
+        }
+    }
+    if (count > 0) return count;
+    int global_count = i + 1;
+    for (int i = 1; i < 20; i++){
+        k = (int)(number / pow(0.1, i)) % 10;
+        global_count += 1;
+        if (k == digit){
+            return global_count;
+            break;
+        }
+    }
+    if (global_count == 0) return -1;
+    return 0;
 }
