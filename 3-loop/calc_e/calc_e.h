fed613e372305b8253e58a687e89e3ffb3652020
NikitaOrehov <Ornik0005@yandex.ru>
2023-11-06 01:57:50 +0300

    06.11.2023

--- 3-loop/calc_e/calc_e.h
+++ 3-loop/calc_e/calc_e.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 unsigned long long calc_e(char order){
-    return 0;
+    int toch = order;
+    double prov = pow(10, toch);
+    double prov1 = 1.0 / prov;
+    double a = 2.718281;
+    for (int i = 1; i < 1000000; i ++){
+        double d = pow((1.0 + 1.0 / i), i);
+        if ((int)(a * prov)  == (int)(d * prov)){
+            return i;
+            break;
+        }
+    }
 }
