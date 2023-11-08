bee913d073ad3492d6035eeac13b12ded4c09f9d
NikitaOrehov <Ornik0005@yandex.ru>
2023-11-09 01:22:51 +0300

    08.11.2023

--- 4-array/little_big_converter/little_big_convert.h
+++ 4-array/little_big_converter/little_big_convert.h
@@ -1,5 +1,11 @@
 #include <math.h>
 
 unsigned int little_big_convert(unsigned int input){
-    return 0;
+    int k0, k1, k2, k3;
+    k3 = input >> 24;
+    k2 = (char)(input >> 16);
+    k1 = (char)(input >> 8);
+    k0 = (char)(input);
+    return k3 | k2 | k1 | k0;
+    
 }
