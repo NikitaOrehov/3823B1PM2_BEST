010cdee0d4e9831f8515a3587498f245b0d82832
NikitaOrehov <Ornik0005@yandex.ru>
2023-11-18 00:45:32 +0300

    ппп

--- 5-pointer/calc_size/calcsize.h
+++ 5-pointer/calc_size/calcsize.h
@@ -1,5 +1,12 @@
 #include <math.h>
 
 int calcsize(void* memory){
-    return 0;
+    int* p = (int*)memory;
+    int count = 1;
+    while ((*(p - 1) != 11) || (*p != 47)){
+        p += 1;
+        count += 1;
+    }
+    return count;
 }
+// 1 2 3 4 5 6
