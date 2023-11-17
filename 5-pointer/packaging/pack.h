51e6357ac744bef3e37a80b6f2902611a76770a3
NikitaOrehov <Ornik0005@yandex.ru>
2023-11-17 20:57:05 +0300

    17.11.2023_1

--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,9 +1,17 @@
 #include <math.h>
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-    return 100;
+    unsigned long long int number = 0, count  = 0;
+    if (size > 8) return 0;
+    for (int i = 0; i < size; i++){
+        number |= ((unsigned long long)(arr[i])) << (i * 8);
+        count += 1;
+    }
+    return number;
 }
 
 unsigned char unpack_ull(unsigned long long input){
-    return 5;
+    unsigned char number;
+    number = (input) >> 32;
+    return number;
 }
