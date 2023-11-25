fe361807d4f6c4b330e6093a9b56bafad531f40b
NikitaOrehov <Ornik0005@yandex.ru>
2023-11-26 01:28:03 +0300

    26.11.2023

--- 6-functions/loops/loops_finder.h
+++ 6-functions/loops/loops_finder.h
@@ -1,5 +1,57 @@
 #include <math.h>
+#include <stdlib.h>
+
+
+int** matrix(int* pic, int size, int* p){
+    size = (int)sqrt(size);
+    int** array = malloc(sizeof(int*) * sqrt(size));
+    for (int i = 0; i < sqrt(size); i++){
+        for (int j = 0; j < sqrt(size); j++){
+            array[i][j] = pic[i * size + j];
+        }
+    }
+    return array;
+}
+
+void start(int** array, int size, int* start_row, int* start_col){
+    for (int i = 0; i < sqrt(size); i++){
+        if (array[i][0] == 1){
+            *start_row = i;
+            *start_col = 0;
+            break;
+        }
+    }
+}
+
+void search(int** array, int* now_row, int* now_col){
+    
+}
 
 void find_loops(int* pic, int size, int* num_loop, int* start_row, int* start_col, int* finish_row, int* finish_col){
+    int* p = malloc(sizeof(int) * size);
+    matrix(pic, size, p);
+    typedef struct coordinates{
+        int row;
+        int col;
+    }coordinates;
+    coordinates array[40];
+    for (int i = 0; i < 40; i++){
+        array[i].row = -1;
+        array[i].col = -1;
+    }
+    array[0].row = *start_row;
+    array[0].col = *start_col;
+    int* now_row, now_col;
+    *now_row = *start_row;
+    // *now_col = *start_col;
+    size = sqrt(size);
+    int flag = 1;
+    while (flag != 0){
+        
+    }
+
+
+    free(p);
+    //free(array);
     return;
 }
--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -2,44 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-void max_min(int* array, int k, int m, int f){
-    srand(time(NULL));
-    int max = 0, min = 1;
-    while ((max - min != f) || (max < min)){
-        max = rand() % k/2;
-        min = rand() % (k/4);
-    }
-    array[0] = min;
-    array[1] = max;
-}
 
-int check(int* array, int* real_size, int m, int k){
-    int sum = 0;
+int* create_array(int *real_size, int number){
+    int* array = malloc(sizeof(int) * *real_size);
     for (int i = 0; i < *real_size; i++){
-        sum += array[*real_size];
+        array[i] = number;
     }
-    if ((sum < k) && (sum > m)) return 1;
-    return 0;
-}
+    return array;
 
-void geterator(int* array, int* real_size, int k, int m){
-    srand(time(NULL));
-    k = (k - array[0] - array[1]);
-    int flag = 1;
-    while (flag != 1){
-        for (int i = 2; i < *real_size - 2; i++){
-            array[*real_size] = (array[0] + 1) + rand() % (array[1] - array[0] - 1);
-        }
-        flag = check(array, real_size, m, k);
-    }
-    
 }
-
 
 int* array_generator(int* real_size, int k, int m, int f){
-    *real_size = 10 + rand() % 91;
-    int* array = malloc(sizeof(int) * *real_size);
-    man_min(array, k, m, f);
-    generator(array, real_size, k, m);
-    return array;
+    int flag = 1, number = 0, difference = 0;
+    int* array;
+    while (flag == 1){
+        for (int i = 10; i < k + 1; i++){
+            if (k % i == 0){
+                flag = 0;
+                *real_size = i;
+                break;
+            }
+        }
+        number = k / *real_size;
+        array = create_array(real_size, number);
+        if (f % 2 == 0){
+            array[0] -= f/2;
+            array[1] += f/2;
+        }
+        else{
+            array[0] -= f/2 - 1;
+            array[1] += f/2;
+        }
+        return array;
+    }
 }
