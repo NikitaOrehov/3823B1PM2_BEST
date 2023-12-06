54b0af6a2ed24c25560dae1e974c03b7b10104de
NikitaOrehov <Ornik0005@yandex.ru>
2023-12-07 01:17:35 +0300

    07.12.2023

--- 7-recursion/new_series/find_series_sum.h
+++ 7-recursion/new_series/find_series_sum.h
@@ -1,9 +1,23 @@
 #include <math.h>
+#include <stdlib.h>
+
+double array_f(double* array, int k){
+    if (k == 0){
+        array[0] = 0;
+        return 0;
+    }
+    double number = (array_f(array, k - 1) + 1) / k;
+    array[k] = number;
+    return array[k];
+}
 
 double find_sum_elements_series(int k){
-    k--;
+    double* array = malloc(sizeof(double) * k);
     if (k == 0) return 0;
-    double sum = 0, number;
-    sum += find_sum_elements_series(k);//////неверно
+    if (k == -1) return -1;
+    double sum = array_f(array, k - 1);
+    for (int i = 0; i < k - 1; i++){
+        sum += array[i];
+    }
     return sum;
 }
--- 8-strings/palindrome/find_palindrom.h
+++ 8-strings/palindrome/find_palindrom.h
@@ -3,6 +3,8 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+
+
 int is_palindrom(char text[]){
     char* texting = malloc(40);
     int count = 0, count2 = 0;
@@ -35,3 +37,43 @@ int is_palindrom(char text[]){
 }
 
 
+
+
+
+
+
+
+
+
+//     int punction(int x){
+//     if (((x >= 32) && (x <= 47)) || ((x >= 58) && (x <= 64)) || ((x >= 91) && (x <= 96)) || ((x >= 123) && (x <= 126))){
+//         return 1;
+//     }
+//     return 0;
+// }
+
+
+// int is_palindrom(char text[]){
+//     int length = strlen(text) - 1, right  = 0, left = 0, flag = 1;
+//     while (left < length - right){
+//         if (punction(text[left]) == 1){
+//             left++;
+//             continue;
+//         }
+//         if (punction(text[length - right]) == 1){
+//             right++;
+//             continue;
+//         }
+
+//         if (text[left] == text[length - right]){
+//             left++;
+//             right++;
+//         }
+//         else{
+//             flag = -1;
+//             break;
+//         }
+//     }
+//     if (flag == -1) return 0;
+//     return 1;
+// }
